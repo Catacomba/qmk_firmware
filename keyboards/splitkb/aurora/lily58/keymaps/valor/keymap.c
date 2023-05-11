19426e3f6950cb21004f79886c73a1b789fb07be
Kevin Šuc <kevinsuc16@gmail.com>
2023-05-12 00:09:47 +0200

    update

--- keyboards/splitkb/aurora/lily58/keymaps/valor/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/valor/keymap.c
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT(
      // XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   , XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX .
     	KC_ESC  , KC_1    , KC_2      , KC_3      , KC_4    , KC_5      ,                     KC_6      , KC_7    , KC_8    , KC_9    , KC_0    , KC_GRV  , 
-    	KC_TAB  , KC_Q    , KC_W      , KC_E      , KC_R    , KC_T      ,                     KC_Y      , KC_U    , KC_I    , KC_O    , KC_P    , KC_MINS , 
+    	KC_TAB  , KC_Q    , KC_W      , KC_E      , KC_R    , KC_T      ,                     KC_Z    , KC_U    , KC_I    , KC_O    , KC_P    , KC_MINS , 
     	KC_LSFT , KC_A    , KC_S      , KC_D      , KC_F    , KC_G      ,                     KC_H      , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT , 
-    	KC_LCTL , KC_Z    , KC_X      , KC_C      , KC_V    , KC_B      , KC_LBRC , KC_RBRC , KC_N      , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_BSLS , 
+    	KC_LCTL , KC_Y    , KC_X      , KC_C      , KC_V    , KC_B      , KC_LBRC , KC_RBRC , KC_N      , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_BSLS , 
 		                                KC_LALT   , KC_LGUI , MO(1)     , KC_SPC  , KC_ENT  , MO(2)     , KC_BSPC , KC_RALT
        ),
     [_LAYER1] = LAYOUT(
@@ -67,10 +67,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_RGB] = LAYOUT(
      // XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX .
-    	XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   ,                     XXXXXXX , XXXXXXX , XXXXXXX , BL_UP   , XXXXXXX , XXXXXXX ,  
-    	XXXXXXX , RGB_M_P , RGB_M_B   , RGB_M_R   , RGB_M_SW, RGB_M_SN  ,                     BL_TOGG , BL_STEP , BL_BRTG , BL_DOWN , XXXXXXX , XXXXXXX , 
-    	XXXXXXX , RGB_M_X , RGB_M_T   , XXXXXXX   , XXXXXXX , XXXXXXX   ,                     RGB_SPI , XXXXXXX , RGB_TOG , RGB_HUI , RGB_SAI , RGB_VAI , 
-    	XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   , XXXXXXX , XXXXXXX , RGB_M_R , RGB_RMOD, RGB_MOD , RGB_HUD , RGB_SAD , RGB_VAD , 
+    	XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , RGB_MODE_BREATHE ,  
+    	XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , RGB_MODE_PLAIN , 
+    	XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   ,                     XXXXXXX , RGB_MOD , RGB_SPI , RGB_HUI , RGB_SAI , RGB_VAI , 
+    	XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   , XXXXXXX , XXXXXXX , XXXXXXX , RGB_RMOD, RGB_SPD , RGB_HUD , RGB_SAD , RGB_VAD , 
     	                                _______   , _______ , _______   , _______ , _______ , _______ , _______ , _______
 	)
 };
